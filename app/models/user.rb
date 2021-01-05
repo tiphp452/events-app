4462c7be23192f3b230eae678542d7a32eda26d7
tiphp452 <tiphp452@gmail.com>
2021-01-05 22:38:08 +0900

    202101052238

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,5 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
   has_many :created_events, class_name: "Event", foreign_key: "owner_id"
+  has_many :tickets
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,11 @@
 Rails.application.routes.draw do
-  resources :events
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions'
   }
   root "welcome#index"
+  resources :events do
+    resources :tickets
+  end
+
 end
