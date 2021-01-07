c188052cd2694600199bd4d34ccfccbd1c38c4b3
tiphp452 <tiphp452@gmail.com>
2021-01-07 22:35:34 +0900

    202101072235

--- config/application.rb
+++ config/application.rb
@@ -12,6 +12,7 @@ module EventsApp
     config.load_defaults 6.0
     config.time_zone = "Tokyo"
     config.i18n.default_locale = :ja
+    config.active_job.queue_adapter = :sidekiq
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
